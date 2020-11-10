5e2cac02af71dedbc337b0cdf707a8eb29b1013f
dawlatzai <dawlatzai.ghousi@gmail.com>
2020-11-10 20:52:36 +0430

    Add performance.now() method for checking and measuring the algorithm performance.!

--- sumUp.js
+++ sumUp.js
@@ -9,3 +9,25 @@ function sumUp(n) {
 }
 
 console.log(sumUp(100))
+
+/** start and end variables for performance issue  */
+let start = 0
+let end = 0
+
+/** performence.now() is js object for checking performance of OS */
+
+start = performance.now()
+sumUp(10)
+end = performance.now()
+
+start = performance.now()
+sumUp(15)
+end = performance.now()
+
+start = performance.now()
+sumUp(100000)
+end = performance.now()
+
+let resultOfPerformance = start - end
+
+console.log(resultOfPerformance)
