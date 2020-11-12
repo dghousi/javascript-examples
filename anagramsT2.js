14a205fb7f19c109dda26628b0f301ba973b3bd8
dawlatzai <dawlatzai.ghousi@gmail.com>
2020-11-12 18:12:26 +0430

    Add anagrams and match each strings with second type

---
+++ anagramsT2.js
@@ -0,0 +1,14 @@
+//anagrams('Test', 'Tset');
+
+function anagrams(stringA, stringB) {
+  // Sanitize string inputs
+  stringA = stringA.toLowerCase().replace(/[\W_]+/g, '')
+  stringB = stringB.toLowerCase().replace(/[\W_]+/g, '')
+
+  if (stringA.length !== stringB.length) return false
+
+  const stringASorted = stringA.split('').sort().join('')
+  const stringBSorted = stringB.split('').sort().join('')
+
+  return stringASorted === stringBSorted
+}
