d605c18a655ec9aa3251e712f17782c8d9ad2d7e
dawlatzai <dawlatzai.ghousi@gmail.com>
2020-11-12 17:40:23 +0430

    Add conditionals and log it

---
+++ condtional.js
@@ -0,0 +1,6 @@
+console.log(['a'] === ['a']) // false beacase it checks the position in memory and see that are located in different position and returns false;
+console.log('a' === 'a') // true
+console.log(true === 1) //false
+console.log(true == 1) //true
+console.log(false == 0) //true
+console.log(false === 0) //false
