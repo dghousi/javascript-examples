c8ca4abeb6dcde8dfe974956a1bf6182cdfeb6e4
dghousi <dawlatzai.ghousi@gmail.com>
2022-08-18 21:56:23 +0430

    add event delegation example

---
+++ event-delefation/scripts.js
@@ -0,0 +1,6 @@
+document.querySelector('#products').addEventListener('click', (event) => {
+    
+    if (event.target.tagName === 'LI') {
+        window.location.href += "#"+event.target.id
+    }
+})
