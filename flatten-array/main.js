c656d5df2d4aa8011bc0789933ef290d810d5133
dghousi <dawlatzai.ghousi@gmail.com>
2022-08-18 22:28:43 +0430

    add flatten array example

--- flatten-array/main.js
+++ flatten-array/main.js
@@ -2,7 +2,7 @@ let arr = [
     [1, 2],
     [3, 4],
     [5, 6][7, 8, 9],
-    [10, 11, 12, 13, 14, 15]
+    [10, [10,99,99], 11, 12, 13, 14, 15, [22,3,5,6,7]]
 ];
 
 
@@ -11,13 +11,13 @@ let arr = [
 
 let flatArray = [].concat.apply([], arr);
 
-console.log(flatArray)
+console.log("The Result T1: \n"+flatArray)
 
 // T2: Using the spread operator
 
 let flatArrayT2 = [].concat(...arr);
 
-console.log(flatArrayT2)
+console.log("The Result T2: \n"+flatArrayT2)
 
 // T3: Using the reduce method
 
@@ -26,4 +26,9 @@ let flatArrayT3 = arr.reduce((acc, curVal) => {
     return acc.concat(curVal)
 }, []);
 
-console.log(flatArrayT3)
+console.log("The Result T3: \n"+flatArrayT3)
+
+// T4: using flat() method
+
+let flatT4 = arr.flat(6)
+console.log("The Result T4: \n"+flatT4)
