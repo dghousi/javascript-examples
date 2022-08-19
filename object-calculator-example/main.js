118fcb10c0d288990da4ab8ffc9a324bda209026
dghousi <dawlatzai.ghousi@gmail.com>
2022-08-19 16:51:20 +0430

    add another example of calc object

--- object-calculator-example/main.js
+++ object-calculator-example/main.js
@@ -30,3 +30,25 @@ console.log(calculator.sum())
 console.log(calculator.subtract())
 console.log(calculator.multiply())
 console.log(calculator.devide())
+
+// another example
+
+let calc = {
+    total: 0,
+    add(a) {
+        this.total += a
+        return this
+    },
+    subtract(a) {
+        this.total -= a
+        return this
+    },
+    multiply(a) {
+        this.total *= a
+        return this
+    },
+}
+
+const result = calc.add(10).subtract(5).multiply(10).add(20)
+
+console.log(result.total)
