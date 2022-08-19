afa367c5865752da40db9f718fa14f873ce403ce
dghousi <dawlatzai.ghousi@gmail.com>
2022-08-19 15:41:01 +0430

    add this keyword examples

--- this-keyword-examples/main.js
+++ this-keyword-examples/main.js
@@ -7,4 +7,27 @@ const person = {
     }
   };
 
-  console.log(this, person.fullName())
+  console.log(person.fullName())
+
+  const user = {
+
+    name: "Jhon",
+    ref : this // in here this is refering to the parent object which is global window object 
+  }
+
+  console.log("\n: "+user.ref.name) // noting return because window object does not have any attribute which is name
+  
+
+  const user1 = {
+
+    name: "Jhon",
+    ref () {
+        return this // in here this is refering to the parent object which is the name and return the name
+    }
+        
+  }
+
+  console.log("\n: "+user1.ref().name) // returning Jhon
+  
+  
+  
