86ea475db0a330ca12f0e27f9996e35247b2de68
dghousi <dawlatzai.ghousi@gmail.com>
2022-08-22 19:14:30 +0430

    add first javascript assignment example

---
+++ javascript-assignment/main.js
@@ -0,0 +1,12 @@
+
+
+let modal = document.querySelector("#results")
+
+setTimeout(function () {
+    alert("BOOM")
+}, 10000);
+
+console.log('Results shown')
+
+// the result `Results shown: immediately`
+
