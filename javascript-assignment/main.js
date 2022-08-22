723cf69fa552791d93b8225d8a6526b030c7e6e2
dghousi <dawlatzai.ghousi@gmail.com>
2022-08-22 22:14:35 +0430

    add first javascript assignment example

--- javascript-assignment/main.js
+++ javascript-assignment/main.js
@@ -3,10 +3,21 @@
 let modal = document.querySelector("#results")
 
 setTimeout(function () {
-    alert("BOOM")
+    console.log("BOOM After 10000")
 }, 10000);
 
 console.log('Results shown')
 
 // the result `Results shown: immediately`
 
+class Person {
+    constructor(name, age)
+    {
+        this.name = name
+        this.age = age
+    }
+}
+
+var person = new Person('Dawlatzai', '27')
+
+console.log(person)
